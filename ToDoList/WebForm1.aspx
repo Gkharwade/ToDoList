09516fa7eea32f35a3a6687c9ef8173ebd712a6b
Gkharwade <gkharwade18@gmail.com>
2025-01-17 23:58:26 +0530

    Add project files.

---
+++ ToDoList/WebForm1.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ToDoList.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="row">
+            <div class="row-cols-3">
+
+                <h2>ToDo List</h2>
+
+
+                <input type="text" id="Task" placeholder="Add your Task" runat="server" cssclass="form-control bg-dark w-25 h-50">
+
+
+                <asp:TextBox ID="txtDate" runat="server" CssClass="m-3" TextMode="Date"></asp:TextBox>
+
+                <asp:Button type="Button" runat="server" CssClass="btn btn-primary m-3 p-2" Text="ADD" OnClick="ADD_btn" />
+            </div>
+        </div>
+
+
+        <asp:GridView ID="GridView1" CssClass="text-center table table-bordered table-hover" runat="server" AutoGenerateColumns="False" OnSelectedIndexChanged="GridView1_SelectedIndexChanged " OnRowDeleting="GridView1_RowDeleting">
+            <Columns>
+                <asp:TemplateField HeaderText="USERID">
+    <ItemTemplate>
+        <asp:Literal runat="server" ID="TASKNO"  Text='<%# Eval("TaskID") %>'></asp:Literal>
+    </ItemTemplate>
+</asp:TemplateField>
+                <asp:TemplateField HeaderText="Task">
+                    <ItemTemplate>
+                        <asp:Literal runat="server" ID="TASKNAME" Text='<%# Eval("Task") %>'></asp:Literal>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="TASKDATE">
+                    <ItemTemplate>
+                        <asp:Literal runat="server" ID="TASKDATE" Text='<%# Eval("TaskDate") %>'></asp:Literal>
+                    </ItemTemplate>
+                </asp:TemplateField>
+                <asp:TemplateField HeaderText="STATUS">
+                    <ItemTemplate>
+                        <asp:Literal runat="server" ID="STATUS" Text='<%# Eval("TaskStat") %>'></asp:Literal>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+                <asp:ButtonField ButtonType="Button" CommandName="SELECT" HeaderText="Done" ShowHeader="True" Text="Done" />
+
+                <asp:ButtonField ButtonType="Button" CommandName="Delete" HeaderText="Delete" ShowHeader="True" Text="Delete " />
+                <asp:CommandField ShowDeleteButton="True" />
+            </Columns>
+        </asp:GridView>
+
+
+
+        <%--<asp:GridView ID="GridView1" CssClass="table table-bordered border-primary table-hover" runat="server" AutoGenerateColumns="false" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <Columns>
+                <asp:TemplateField HeaderText="Tasks"></asp:TemplateField>
+                <itemtemplate>
+                    <itemtemplate>
+                        <asp:Literal ID="LiteralStdName" runat="server" Text='<%# Eval("S_name") %>'></asp:Literal>
+                    </itemtemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="Marks">
+                        <itemtemplate>
+                            <asp:Literal ID="LiteralMarks" runat="server" Text='<%# Eval("marks") %>'></asp:Literal>
+                        </itemtemplate>
+                    </asp:TemplateField>
+                    <asp:TemplateField HeaderText="rollno">
+                        <itemtemplate>
+                            <asp:Literal ID="LiteralRollNo" runat="server" Text='<%# Eval("roll_no") %>'></asp:Literal>
+                        </itemtemplate>
+                    </asp:TemplateField>
+                </itemtemplate>
+
+            </Columns>
+
+
+        </asp:GridView>--%>
+    </div>
+
+</asp:Content>
---
+++ ToDoList/WebForm1.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Windows;
+
+namespace ToDoList
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+    
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            getbind();
+            
+
+        }
+        protected void ADD_btn(object sender, EventArgs e)
+        {
+            try
+            {
+                string TaskName= Task.Value;
+                string Date = txtDate.Text;
+                string query = "Insert into ToDo ( Task,TaskDate) values (@taskname,@tskdate) ";
+                SqlConnection conn = new SqlConnection(connectionString);
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@taskname",TaskName );
+                cmd.Parameters.AddWithValue("@tskdate", Date);
+                int RowsAffected= cmd.ExecuteNonQuery();
+                conn.Close();
+                if (RowsAffected > 0)
+                {
+                    MessageBox.Show("inserted");
+                    getbind();
+                }
+                else
+                {
+                    MessageBox.Show("not inserted");
+                }
+                        
+                        
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void getbind()
+        {
+            try
+            {
+                string querry = "select * from ToDo WHERE TaskStat='Pending'";
+                SqlConnection con = new SqlConnection(connectionString);
+                SqlCommand cmd =new SqlCommand(querry, con);
+                con.Open();
+                SqlDataAdapter adapter= new SqlDataAdapter(cmd);
+                DataSet dataSet = new DataSet();
+                adapter.Fill(dataSet);
+                 
+                if(dataSet.Tables.Count >= 0)
+                {
+                    if(dataSet.Tables[0].Rows.Count >= 0)
+                    {
+                        GridView1.DataSource = dataSet.Tables[0];
+                         GridView1.DataBind();
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int TID=0 ;
+            try
+            {
+                GridViewRow selectrow = GridView1.SelectedRow;
+                
+                if (selectrow !=null)
+                {
+                    Literal LiteralID = (Literal)selectrow.FindControl("TASKNO");
+                    TID = Convert.ToInt32(LiteralID.Text.Trim());
+                   
+                }
+                SqlConnection con = new SqlConnection(connectionString);
+                string querry = "update ToDo set TaskStat='DONE' WHERE TaskID=@TaskID ";
+                SqlCommand cmd = new SqlCommand(querry, con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@TaskID" ,TID);
+                int RowsAffected = cmd.ExecuteNonQuery();
+                con.Close();
+                if (RowsAffected > 0)
+                {
+                    MessageBox.Show("YOUR TASK IS COMPLEDTED");
+                    getbind();
+                }
+                else
+
+                {
+                    MessageBox.Show("TASK NOT COMPLETED");
+                }
+
+            }
+            catch(Exception Ex) {
+            MessageBox.Show(Ex.Message);
+            }
+        }
+        protected void GridView1_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            try
+            {
+                // Get the TaskID (primary key) of the selected row
+                int TaskID = Convert.ToInt32(GridView1.DataKeys[e.RowIndex].Value);
+
+                // SQL query to delete the row from the database
+                string query = "DELETE FROM ToDo WHERE TaskID = @TaskID";
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@TaskID", TaskID);
+
+                        con.Open();
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        con.Close();
+
+                        // Display success message and refresh the GridView
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("Task deleted successfully.");
+                            getbind(); // Rebind the GridView
+                        }
+                        else
+                        {
+                            MessageBox.Show("Task not found or could not be deleted.");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+    }
+
+}
